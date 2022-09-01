8b8d996853172ea14fd87b0c56b7208ae41a8060
Reks <fahadadsani@gmail.com>
2022-09-01 09:20:23 +0300

    commit 4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 //fetch('https://jsonplaceholder.typicode.com/users/1')
-const emailRef = document.querySelector('.email')
+// const emailRef = document.querySelector('.email')
 
 /* DONT DO THEN U FUCKING IDIOT */
 
@@ -31,7 +31,47 @@ const emailRef = document.querySelector('.email')
 
 // main()
 
+const statusRef = document.querySelector('.status')
+const videoRef = document.querySelector('.e')
+
+function getSubStatus(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve('VIP')
+        }, 2000)
+    })
+}
 
 
+//async method
+
+function getVideo(subStatus){
+    return new Promise((resolve, reject) => {
+        if (subStatus === 'VIP'){
+            resolve('Show video')
+        }
+        else if (subStatus === 'FREE'){
+            resolve('Show trailer')
+        }
+        else{
+            reject('no')
+        }  
+    })
+}
+
+
+async function main(){
+    const status = await getSubStatus()
+    statusRef.innerHTML = status
+    try{
+        console.log(await getVideo(status))
+    }
+    catch (e){
+        console.log(e)
+        videoRef.innerHTML = e
+    }
+}
+
+main()
 
 
