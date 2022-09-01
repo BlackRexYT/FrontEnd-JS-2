658dfe27eb0fc31ac703e6e5ab72060c34c7cdc0
Reks <fahadadsani@gmail.com>
2022-09-01 08:54:29 +0300

    commit 2

--- script.js
+++ script.js
@@ -3,16 +3,34 @@
 
 //fetch('https://jsonplaceholder.typicode.com/users/1')
 const emailRef = document.querySelector('.email')
-console.log(emailRef)
+
 
 
 //1. then
-fetch('https://jsonplaceholder.typicode.com/users/1').then((response) => {
-    response.json().then(data => {
-        console.log(data)
-    })
-})
+
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+// .then((response) => {
+//     return response.json()
+// })
+// .then(data => {
+//     console.log(data)
+//     emailRef.innerHTML = data.email
+// })
 
 
-//2. async await
+
+//2. async//await
+
+async function main(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+    const data = await response.json()
+    emailRef.innerHTML = data.email
+    
+}
+
+main()
+
+
+
+
 
